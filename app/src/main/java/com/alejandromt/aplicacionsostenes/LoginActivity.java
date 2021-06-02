45fba3f48a38b125ff1bfafc8ae98ab939aa7d24
Alejandro Martinez <alex252478@gmail.com>
2021-06-02 00:43:23 -0500

    2020.06.1

---
+++ app/src/main/java/com/alejandromt/aplicacionsostenes/LoginActivity.java
@@ -0,0 +1,28 @@
+package com.alejandromt.aplicacionsostenes;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+
+import androidx.appcompat.app.AlertDialog;
+import androidx.appcompat.app.AppCompatActivity;
+
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.Statement;
+
+public class LoginActivity extends AppCompatActivity {
+
+    Connection connect;
+    String ConnectionResult = "";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+
+
+
+}
